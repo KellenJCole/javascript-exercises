e03280541363c4468b7150c8d09fc918f272595f
Kellen Cole <kellencole03@gmail.com>
2023-07-14 02:33:36 -0700

    Solve exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(passed) {
+  if (passed[0] == undefined) return 0;
+	let total = 0;
+	passed.forEach(element => {
+    total += element;
+  });
+  return total;
 };
 
 const multiply = function() {
-
+  if(arguments[0] == undefined) return 0;
+  let total = 1;
+	for (let i = 0; i < arguments.length; i++) {
+    total *= arguments[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let answer = n;
+	if (n === 1 || n === 0) {
+    return 1;
+  }
+  answer *= factorial (n - 1);
+  return answer;
+
 };
 
 // Do not edit below this line
