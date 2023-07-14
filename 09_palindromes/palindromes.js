2ac83f481dbf0e504426bf692f3f5b73748164a1
Kellen Cole <kellencole03@gmail.com>
2023-07-14 02:47:01 -0700

    Solve exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+let isAlpha = function(input) {
+    return /^[A-Z\d]$/i.test(input);
+}
 
+const palindromes = function (phrase) {
+    let alphaOnly = "";
+    const arrayForm = phrase.split("");
+    for (let i = 0; i < arrayForm.length; i++) {
+        if (isAlpha(arrayForm[i])) alphaOnly += arrayForm[i].toLowerCase();
+    }
+    let leftPointer, rightPointer;
+    leftPointer = 0;
+    rightPointer = alphaOnly.length - 1;
+    
+
+    const alphaArrayForm = alphaOnly.split("");
+
+    while (leftPointer < rightPointer) {
+        if (alphaArrayForm[leftPointer] != alphaArrayForm[rightPointer]) return false;
+        leftPointer++;
+        rightPointer--;
+    }
+    return true;
 };
 
 // Do not edit below this line
